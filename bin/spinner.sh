e60bfe3dd4879f8331434d276b81d622584e1660
Grigorii Okhmak <grigorii.okhmak@mera.com>
2021-03-26 18:14:10 +0300

    Spinner additions

--- bin/spinner.sh
+++ bin/spinner.sh
@@ -4,6 +4,7 @@ spinner() {
     local pid=$1
     local delay=0.75
     local spinstr='|/-\'
+    tput civis
     while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
         local temp=${spinstr#?}
         printf " [%c]  " "$spinstr"
@@ -11,11 +12,12 @@ spinner() {
         sleep $delay
         printf "\b\b\b\b\b\b"
     done
+    tput cnorm
     printf "    \b\b\b\b"
 }
 
 long_process_imitation() {
-    echo -n "Doing something important, please wait... "
+    echo -n "Doing something important, please wait..."
     sleep 10
 }
 
