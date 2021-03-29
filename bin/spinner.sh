cfce18130f02dd675df1a38240001de5c024d881
Grigorii Okhmak <grigorii.okhmak@mera.com>
2021-03-29 10:50:34 +0300

    Improved spinner

--- bin/spinner.sh
+++ bin/spinner.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+readonly WAITING_TIME=2
+
 spinner() {
     local pid=$1
     local delay=0.75
@@ -16,11 +18,38 @@ spinner() {
     printf "    \b\b\b\b"
 }
 
+spinner_with_timer() {
+    local pid=$1
+    local custom_line=${2:-}
+    local delay=0.75
+    local spinstr='|/-\'
+    local time_format=%X
+    local time_line
+    local line
+    local line_length
+    tput civis
+    while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
+        local temp=${spinstr#?}
+        time_line=$(date +%X)
+        printf -v line ' %s (%c)  %s' "${time_line}" "$spinstr" "$custom_line"
+        line_length=${#line}
+        printf "%s" "${line}"
+        local spinstr=$temp${spinstr%"$temp"}
+        sleep $delay
+        printf -v line '%*s' ${line_length}
+        line=${line// /'\b'}
+        printf "$line"
+    done
+    tput el
+    tput cnorm
+}
+
 long_process_imitation() {
     echo -n "Doing something important, please wait..."
-    sleep 10
+    sleep $WAITING_TIME
 }
 
 (long_process_imitation) &
-spinner $!
+#spinner $!
+spinner_with_timer $! "Please, wait!"
 echo
